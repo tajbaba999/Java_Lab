0f6538dd9a0904163da2a03116940c75a729ef90
tajbaba999 <tajbaba153@gmail.com>
2023-02-20 22:28:15 +0530

    key event listner

---
+++ KeyList.java
@@ -0,0 +1,46 @@
+//KeyListener
+import java.awt.*;  
+import java.awt.event.*; 
+
+public class KeyList extends Frame implements KeyListener{
+   
+
+ Label l;    
+    TextArea area;    
+
+    KeyList() {    
+       
+        l = new Label();    
+
+        l.setBounds(20, 50, 100, 20);    
+
+        area = new TextArea();    
+  
+        area.setBounds(20, 80, 300, 300);    
+ 
+        area.addKeyListener(this);  
+
+        add(l);  
+	    add(area);    
+        setSize(400, 400);    
+        setLayout(null);    
+        setVisible(true);    
+    }    
+
+    public void keyPressed (KeyEvent e) {    
+        l.setText("Key Pressed");    
+    }    
+
+    public void keyReleased (KeyEvent e) {    
+        l.setText("Key Released");    
+    }    
+
+    public void keyTyped (KeyEvent e) {    
+        l.setText("Key Typed");    
+    }    
+
+  public static void main(String[] args) {
+    new KeyList();
+  }   
+}
+
